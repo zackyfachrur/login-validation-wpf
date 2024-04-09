d4762e560faf7d60079159f1fac013ca8b2ff0b0
zackyfachrur <mochzackyaja@gmail.com>
2024-04-09 23:02:34 +0700

    add sign in

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,19 @@
+using System.Media;
+using System.Windows.Forms.VisualStyles;
+
 namespace halamanLogin
 {
     public partial class Form1 : Form
     {
+
         // Private Variables Register
         private string CreateUser;
         private string CreateName;
         private string CreatePassword;
         private GroupBox groupRegister;
         // End
+
+
 
         public Form1()
         {
@@ -18,16 +24,15 @@ namespace halamanLogin
         // Form1 On Load
         public void Form1_Load(object sender, EventArgs e)
         {
-            dataLogin();
-            textBoxCreateUser.Select();
-            groupLogin.Hide();
+            textBoxCreateEmail.Select();
+            groupBoxSignIn.Hide();
         }
         // End
 
         // Data Register
         private void dataRegister()
         {
-            CreateUser = textBoxCreateUser.Text;
+            CreateUser = textBoxCreateEmail.Text;
             CreateName = textBoxCreateName.Text;
             CreatePassword = textBoxCreatePassword.Text;
             groupRegister = groupBoxRegister;
@@ -35,28 +40,19 @@ namespace halamanLogin
         // End
 
 
-        // Data Login
-        private void dataLogin()
-        {
-            LoginName = textBoxName.Text;
-            LoginPassword = textBoxPassword.Text;
-            groupLogin = groupBoxLogin;
-        }
-        // End
-
         // Create Button Click
         private void buttonAcceptRegister_Click(object sender, EventArgs e)
         {
             dataRegister();
-            if (!string.IsNullOrEmpty(textBoxCreateUser.Text) &&
+            if (!string.IsNullOrEmpty(textBoxCreateEmail.Text) &&
                 !string.IsNullOrEmpty(textBoxCreateName.Text) &&
                 !string.IsNullOrEmpty(textBoxCreatePassword.Text))
             {
                 MessageBox.Show("Akun Anda Telah Dibuat!", "Register Successfully",
                 MessageBoxButtons.OK, MessageBoxIcon.Information);
                 groupRegister.Hide();
-                groupBoxLogin.Show();
-                textBoxName.Select();
+                groupBoxSignIn.Show();
+                textBoxEmailLogin.Select();
             }
             else
             {
@@ -66,23 +62,16 @@ namespace halamanLogin
         }
         // End
 
-        //Private Variables Login
-        private string LoginName;
-        private string LoginPassword;
-        private GroupBox groupLogin;
-        // End
-
 
         // Button Accept Login Click
-        private void buttonAcceptLogin_Click(object sender, EventArgs e)
+        private void buttonAcceptSignIn_Click(object sender, EventArgs e)
         {
             dataRegister();
-            dataLogin();
-            if (!string.IsNullOrEmpty(textBoxName.Text) &&
-                !string.IsNullOrEmpty(textBoxPassword.Text) && 
-                textBoxName.Text == textBoxCreateName.Text &&
-                textBoxPassword.Text == textBoxCreatePassword.Text
-                ) 
+            if (!string.IsNullOrEmpty(textBoxEmailLogin.Text) &&
+                !string.IsNullOrEmpty(textBoxPasswordLogin.Text) &&
+                textBoxEmailLogin.Text == textBoxCreateEmail.Text &&
+                textBoxPasswordLogin.Text == textBoxCreatePassword.Text
+                )
             {
                 MessageBox.Show("Selamat Anda Telah Login!", "Login Successfully",
                 MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -97,5 +86,88 @@ namespace halamanLogin
             }
         }
         // End
+
+        // Button Styling
+        private void buttonAcceptRegister_MouseEnter(object sender, EventArgs e)
+        {
+            buttonAcceptRegister.BackColor = Color.Black;
+            buttonAcceptRegister.ForeColor = SystemColors.Highlight;
+            buttonAcceptRegister.Font = new Font(buttonAcceptRegister.Font, FontStyle.Underline);
+        }
+
+        private void buttonAcceptRegister_MouseLeave(object sender, EventArgs e)
+        {
+            buttonAcceptRegister.BackColor = SystemColors.Highlight;
+            buttonAcceptRegister.ForeColor = Color.Black;
+            buttonAcceptRegister.Font = new Font(buttonAcceptRegister.Font, FontStyle.Bold);
+        }
+
+        private void buttonCancelRegister_MouseEnter(object sender, EventArgs e)
+        {
+            buttonCancelRegister.BackColor = Color.Black;
+            buttonCancelRegister.ForeColor = SystemColors.Highlight;
+            buttonCancelRegister.Font = new Font(buttonCancelRegister.Font, FontStyle.Underline);
+        }
+
+        private void buttonCancelRegister_MouseLeave(object sender, EventArgs e)
+        {
+            buttonCancelRegister.BackColor = SystemColors.Highlight;
+            buttonCancelRegister.ForeColor = Color.Black;
+            buttonCancelRegister.Font = new Font(buttonCancelRegister.Font, FontStyle.Bold);
+        }
+        private void buttonCancelRegister_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void labelDontHaveAccount_Click(object sender, EventArgs e)
+        {
+            groupBoxRegister.Show();
+            groupBoxSignIn.Hide();
+        }
+
+        private void labelDontHaveAccount_MouseEnter(object sender, EventArgs e)
+        {
+            labelDontHaveAccount.Font = new Font(labelDontHaveAccount.Font, FontStyle.Underline);
+        }
+        private void labelDontHaveAccount_MouseLeave(object sender, EventArgs e)
+        {
+            labelDontHaveAccount.Font = new Font(labelDontHaveAccount.Font, FontStyle.Bold);
+        }
+
+        private void buttonAcceptSignIn_MouseEnter(object sender, EventArgs e)
+        {
+            buttonAcceptSignIn.BackColor = Color.Black;
+            buttonAcceptSignIn.ForeColor = SystemColors.Highlight;
+            buttonAcceptSignIn.Font = new Font(buttonAcceptSignIn.Font, FontStyle.Underline);
+        }
+        private void buttonAcceptSignIn_MouseLeave(object sender, EventArgs e)
+        {
+            buttonAcceptSignIn.BackColor = SystemColors.Highlight;
+            buttonAcceptSignIn.ForeColor = Color.Black;
+            buttonAcceptSignIn.Font = new Font(buttonAcceptSignIn.Font, FontStyle.Bold);
+        }
+
+        private void ButtonCancelSignIn_Click(object sender, EventArgs e)
+        {
+            groupBoxRegister.Show();
+            groupBoxSignIn.Hide();
+        }
+
+        private void ButtonCancelSignIn_MouseEnter(object sender, EventArgs e)
+        {
+            ButtonCancelSignIn.BackColor = Color.Black;
+            ButtonCancelSignIn.ForeColor = SystemColors.Highlight;
+            ButtonCancelSignIn.Font = new Font(ButtonCancelSignIn.Font, FontStyle.Underline);
+        }
+        private void ButtonCancelSignIn_MouseLeave(object sender, EventArgs e)
+        {
+            ButtonCancelSignIn.BackColor = SystemColors.Highlight;
+            ButtonCancelSignIn.ForeColor = Color.Black;
+            ButtonCancelSignIn.Font = new Font(ButtonCancelSignIn.Font, FontStyle.Bold);
+        }
+        // End
+
+
     }
 }
